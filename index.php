46376c5775449cb6969e6c8fe3b588337d8efa79
danilojiji <125054381@upq.edu.mx>
2026-09-21 16:29:15 -0600

    estilos de php

---
+++ index.php
@@ -0,0 +1,75 @@
+<!-- <?php
+echo "Hola Mundo!";
+?>
+ -->
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+h1{
+    color: blue;
+}
+h2{
+    color: black;
+}
+h3{
+    color: red;
+}
+h4{
+    color: orange;
+}
+h5{
+    color: #b65df1;
+}
+.section {
+          background-color: #78ff7f;
+          padding: 20px 40px;
+         
+ margin-top: 20px; /* Arriba */
+margin-right: 30px; /* Derecha */
+margin-bottom: 40px; /* Abajo */
+margin-left: 50px; /* Izquierda */
+border: 10px solid #000000;
+}
+
+.rosa {
+    background-color: #f569e2;
+padding-top: 20px; /* Arriba */
+padding-right: 40px; /* Derecha */
+padding-bottom: 40px; /* Abajo */
+padding-left: 40px; /* Izquierda */
+
+margin-top: 20px; /* Arriba */
+margin-right: 30px; /* Derecha */
+margin-bottom: 40px; /* Abajo */
+margin-left: 50px; /* Izquierda */
+border: 10px dashed #000000;
+}
+
+    </style>
+ </head>
+
+ <body>
+    <h1>hola crayola</h1>
+<h2>hola crayola</h2>
+<h3>hola crayola</h3>
+<h4>hola crayola</h4>
+<h5>hola crayola</h5>
+
+<div class="section">
+    <h2>Ttitulo del parrafo</h2>
+    <p>lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</div>
+
+<div class="rosa">
+    <h2>Ttitulo del parrafo</h2>
+    <p>lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</div>
+  
+ </body>
+
+ </html>
